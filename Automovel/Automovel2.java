e50702944845321fc9d5bfa7b670be9666ef1fd5
Lenonp <lenon.prates@hotmail.com>
2023-08-28 13:34:45 -0300

    First Test

---
+++ Automovel/Automovel2.java
@@ -0,0 +1,14 @@
+package Automovel;
+
+public class Automovel2 {
+    
+    String combustivel;
+
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
+}
